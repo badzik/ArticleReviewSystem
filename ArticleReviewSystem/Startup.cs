9a314e8f65e7da4a1f7c3cee5383a375c86803ce
Tomasz Jurkowski <jurek993@outlook.com>
2017-10-19 22:40:13 +0200

    Initial Commit. Added example template, but it does not work yet

---
+++ ArticleReviewSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArticleReviewSystem.Startup))]
+namespace ArticleReviewSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
