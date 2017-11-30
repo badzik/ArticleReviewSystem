469cf01703001d533d080b90b4086a4213675a10
Tomasz Jurkowski <jurek993@outlook.com>
2017-11-30 19:12:48 +0100

    Added New Controller - ReviewController

---
+++ ArticleReviewSystem/Controllers/ReviewController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArticleReviewSystem.Controllers
+{
+    public class ReviewController : Controller
+    {
+        // GET: Review
+        public ActionResult ArticlesReviewers()
+        {
+
+            return View();
+        }
+
+  
+}
